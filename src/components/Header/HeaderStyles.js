3e0413abaf9f481324081d49d239f474b59a8a80
Vishnu Dutta <92321047+vishnudutta1@users.noreply.github.com>
2022-01-16 16:52:36 +0530

    Update HeaderStyles.js

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -10,6 +10,7 @@ export const Container = styled.div`
   padding: 1rem;
   padding-top: 2rem;
   position : fixed;
+  background-color : red;
   
   @media ${(props) => props.theme.breakpoints.sm} {
     display: grid;
